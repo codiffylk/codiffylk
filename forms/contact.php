81d89cff4fa40bd50657480298dfec8085301b56
codiffylk <141122912+codiffylk@users.noreply.github.com>
2023-08-02 21:30:42 +0530

    contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,33 +1,57 @@
 <?php
+	// First of all, we need to check if there is a post request.
+	if(isset($_POST['message'])){
+		/* if we have a post request, we have to decode the json string that
+		we are receiving */
+		$data = json_decode($_POST['message']);
+		/* The decoded json string is now a php object */
 
-$receiving_email_address = 'codiffylk@gmail.com';
+		/* Next i loop trough the object to check for empty fields.
+		I know that we have done this check in the javascript file, but never rely on front end validation.
+		Front end validation is only done for a better user experience. */
+		foreach ($data as $value) {
+			if(empty(trim($value))){
+				/* If there is an empty field, we send a message back to the javascript file. */
+				echo "Error: All fields are required";
+				exit(); /* and we stop the script here. */
+			}
+		}
+		/* If there are no empty fields, i create new variables containing the input values
+		just to keep our script simple, so we can understand what is going on. */
+		$firstname =  $data->name; /* Remember that $data is an object now */
+		$email =  $data->email;
+		$message = $data->message;
+		/* Now i will check if the incoming email's value is valid */
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+			/* if email is not valid, we send a server response with an error message */
+			echo "Error: Please enter a valid email";
+			exit();
+		}
 
-if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-include( $php_email_form );
-} else {
-die( 'Unable to load the "PHP Email Form" Library!');
-}
+		/* Now since we are here in this line, our incoming data are correct
+		and we are going to send the email. */
+		
+		
+		/* SET THE EMAIL ADDRESS YOU WANT TO RECEIVE THE MESSAGES  */
+		/* =============================================== */
+		$to = "contact@codiffylk.com";
+		/* =============================================== */
 
-$contact = new PHP_Email_Form;
-$contact->ajax = true;
-
-$contact->to = $receiving_email_address;
-$contact->from_name = $_POST['name'];
-$contact->from_email = $_POST['email'];
-$contact->subject = $_POST['subject'];
-
-
-$contact->smtp = array(
-'host' => 'example.com',
-'username' => 'example',
-'password' => 'pass',
-'port' => '587'
-);
-
-
-$contact->add_message( $_POST['name'], 'From');
-$contact->add_message( $_POST['email'], 'Email');
-$contact->add_message( $_POST['message'], 'Message', 10);
-
-echo $contact->send();
-?>
+		
+		/* Next we have to add a subject */
+		$subject = $firstname . " has a question for you";
+		/* Next we are setting some basic headers */
+		$headers = "MIME-Version: 1.0" . "\r\n";
+		$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+		/* We have to add also a from header, because we want to know the email address from
+		the user who is contacting us. */
+		$headers .= 'From: '. $email . "\r\n";
+		/* Next we are sending the email */
+		$send = mail($to,$subject,$message, $headers);
+		/* And last we check if the mail() function was successful*/
+		if(!$send){
+			echo "Εrror: Message not send. Please try again";
+		}else{
+			echo "Message was send successfully";
+		}
+	}
